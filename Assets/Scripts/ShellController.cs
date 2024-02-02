724c5d6ac98b1a530ef46f1cc9d53abf12f091bd
YU <suk99yu66@gmail.com>
2024-02-02 14:37:45 +0900

    Initial commit

---
+++ Assets/Scripts/ShellController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellController : MonoBehaviour
+{
+    public float deleteTime = 3.0f; // 제거할 시간 지정
+
+   
+    void Start()
+    {
+        Destroy(gameObject, deleteTime); // 제거 설정
+    }
+
+    
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject); // 게임 오브젝트에  접촉하면 제거
+    }
+}
---
+++ Assets/Scripts/UI/TitleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    public GameObject startButton;
+
+    
+
+   
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            ChangeScene cs = startButton.GetComponent<ChangeScene>();
+            if(cs != null)
+            {
+                cs.Load();
+            }
+        }
+    }
+}
